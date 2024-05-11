c8070ca199a0dbfd2ff783ba8cdbe0c46c1f0d4a
likalizer <lika.lika1823@gmail.com>
2024-05-11 12:15:01 +0300

    Завдання 8.3 виконано
    є проблеми з Profile View

--- Lesson11HW/Source/Screen/ContactUsViewController.swift
+++ Lesson11HW/Source/Screen/ContactUsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ContactUsViewController: BaseViewController {
+class ContactUsViewController: BaseViewController, UITextViewDelegate {
     
     
     @IBOutlet weak var messageTextView: UITextView!
@@ -16,8 +16,41 @@ class ContactUsViewController: BaseViewController {
     @IBOutlet weak var submitButton: UIButton!
     
     
-    override func viewDidLoad() {        super.viewDidLoad()
-    
-    }
-}
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            setupTextView()
+        }
+        
+        private func setupTextView() {
+            messageTextView.delegate = self
+            updatePlaceholder()
+            updateCharacterCount()
+            updateSubmitButton()
+        }
 
+    @objc func textViewDidChange(_ textView: UITextView) {
+            updatePlaceholder()
+            updateCharacterCount()
+            updateSubmitButton()
+        }
+
+    @objc(textView:shouldChangeTextInRange:replacementText:) func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            let currentText = textView.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+            let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+            return updatedText.count <= 320
+        }
+
+        private func updatePlaceholder() {
+            placeholderLabel.isHidden = !messageTextView.text.isEmpty
+        }
+
+        private func updateCharacterCount() {
+            let count = messageTextView.text.count
+            characterCountLabel.text = "\(count)/320"
+        }
+
+        private func updateSubmitButton() {
+            submitButton.isEnabled = messageTextView.text.count >= 15
+        }
+    }
